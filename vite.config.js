28f6a546d609a91d971456182fcbff2e6226a164
Powza72 <manu.aon212@gmail.com>
2025-05-13 19:47:47 +0700

    แก้ไขไฟล์ [ชื่อไฟล์] หรือ [รายละเอียด]

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Score-Board/',  // แก้เป็นชื่อ repo ของมึง
   plugins: [
     react(),
     tailwindcss(),
